cb3541c7fe27d6f03589aa808ea9115ed2bae829
ml13c <147662222+ml13c@users.noreply.github.com>
2025-04-10 20:15:13 -0500

    Update one.cpp

--- Question_Solutions/one.cpp
+++ Question_Solutions/one.cpp
@@ -1,1 +1,4 @@
-//c++ code solution to question 1
+/*c++ code solution to question 1
+I am in city “A”, can I fly to city “B” with less than x connections? Give me the
+route with the smallest number of connections or tell me there is no such a route
+*/
