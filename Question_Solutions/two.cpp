bfc0e7832d55b5c2ad5c1e1b9fdd24dcb1c8c66b
ml13c <147662222+ml13c@users.noreply.github.com>
2025-04-10 20:15:52 -0500

    Update two.cpp

--- Question_Solutions/two.cpp
+++ Question_Solutions/two.cpp
@@ -1,1 +1,5 @@
-//c++ solution to question 2
+/*c++ solution to question 2
+Give me the route with the smallest number of connections from city “A” to city
+“D” through city “B” and “C”. (the order of “B” and “C” is not important). Or
+tell me there is no such a route
+*/
