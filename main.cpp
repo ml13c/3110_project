d6f4a37bed5c0ed09974e62ee47e68452196527b
haleyk0 <haleyjkemail@gmail.com>
2025-04-17 19:07:37 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,7 +6,48 @@
 
 using namespace std;
 
+void readFile();
+
 int main (){
+  //readFile();
+  bool running = true;
+  int choice;
+  while (running){
+    cout << "1: Smallest route" << endl;
+    cout << "2: Smallest route through other cities" << endl;
+    cout << "3: Visit all cities and come back to the start" << endl;
+    cout << "4: Find optimal meeting city between 3" << endl;
+    cout << "5: Exit"<< endl;
+    cout << "-->";
+    cin >> choice;
+    cout << endl;
+
+    switch(choice){
+      case 1:
+
+      break;
+      case 2:
+
+      break;
+      case 3:
+
+      break;
+      case 4:
+
+      break;
+      case 5:
+        running = false;
+      break;
+      default:
+        running = false;
+    }
+  }
+  
+
+  return 0;
+}
+
+void readFile(){
   //open flight.txt
   ifstream file; 
   file.open("flight.txt");
@@ -27,23 +68,19 @@ int main (){
 
     //skip "From:" and "To"
     if (line.find("From:") == 0) {
-        string from = line.substr(6); // Skip "From: "
-          from = from.substr(from.find_first_not_of(" \t"));
-          cout << from << endl; //FROM CITY
-          read = false;
-          continue;
-        continue;
+      string from = line.substr(6); // Skip "From: "
+      from = from.substr(from.find_first_not_of(" \t"));
+      cout << from << endl; //FROM CITY (this line will be used to input into data structure)
+      read = false;
+      continue;
     }
     if (line.find("To") == 0) {
       read = true;
       continue;
     }
-
     if (read && !line.empty()) {
-      cout << line << endl; //TO CITIES
+      cout << line << endl; //TO CITIES(this line will be used to input into data structure)
     }
   }
   file.close(); //close flight.txt
-
-  return 0;
 }
